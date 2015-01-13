b7c365e8b6d5cadbb2f7d84745775aa161d6d140
Venkatesh S <svenk@rocketmail.com>
2015-01-13 19:25:49 +0530

    sitevariable

--- includes/sitevariables.php
+++ includes/sitevariables.php
@@ -17,7 +17,7 @@ $bomenquirynumber="bm".$randomkey;
 $procurementmanagementenquirynumber="pm".$randomkey;
 
 /*server link*/
-$serverlink="117.218.227.143:”4567;
+$serverlink="117.218.227.143:4567”;
 //$serverlink="localhost:4567";
 
 /*page tabs*/
